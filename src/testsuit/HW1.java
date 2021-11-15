aa267ed033921a836414bb6586898cd226fcc4c1
rinku031078 <anandtri78@gmail.com>
2021-11-15 19:58:21 +0000

    Amendment added in class HW1

--- src/testsuit/HW1.java
+++ src/testsuit/HW1.java
@@ -28,43 +28,45 @@ public class HW1 extends BaseTest{
         WebElement two = driver.findElement(By.xpath("//h1[contains(text(),'Computers')]"));
         String actuelResult = two.getText();
         Assert.assertEquals("Your are not on Computer Page", expectedResult, actuelResult);
-        stop();
-        start();
+        driver.navigate().to("https://demo.nopcommerce.com/");
+
+
+
         driver.findElement(By.linkText("Electronics")).click();
         String expectedResult1 = "Electronics";
         WebElement three = driver.findElement(By.xpath("//h1[contains(text(),'Electronics')]"));
         String actuelResult1 = three.getText();
         Assert.assertEquals("Your are not on Computer Page", expectedResult1, actuelResult1);
-        stop();
-        start();
+
+
         driver.findElement(By.linkText("Apparel")).click();
         String expectedResult2 = "Apparel";
         WebElement four = driver.findElement(By.xpath("//h1[contains(text(),'Apparel')]"));
         String actuelResult2 = four.getText();
         Assert.assertEquals("Your are not on Computer Page", expectedResult2, actuelResult2);
-        stop();
-        start();
+
+
         driver.findElement(By.linkText("Digital downloads")).click();
         String expectedResult3 = "Digital downloads";
         WebElement five = driver.findElement(By.xpath("//h1[contains(text(),'Digital downloads')]"));
         String actuelResult3 = five.getText();
         Assert.assertEquals("Your are not on Computer Page", expectedResult3, actuelResult3);
-        stop();
-        start();
+
+
         driver.findElement(By.linkText("Books")).click();
         String expectedResult4 = "Books";
         WebElement six = driver.findElement(By.xpath("//h1[contains(text(),'Books')]"));
         String actuelResult4 = six.getText();
         Assert.assertEquals("Your are not on Computer Page", expectedResult4, actuelResult4);
-        stop();
-        start();
+
+
         driver.findElement(By.linkText("Jewelry")).click();
         String expectedResult5 = "Jewelry";
         WebElement seven = driver.findElement(By.xpath("//h1[contains(text(),'Jewelry')]"));
         String actuelResult5 = seven.getText();
         Assert.assertEquals("Your are not on Computer Page", expectedResult5, actuelResult5);
-        stop();
-        start();
+
+        
         driver.findElement(By.linkText("Gift Cards")).click();
         String expectedResult6 = "Gift Cards";
         WebElement eight = driver.findElement(By.xpath("//h1[contains(text(),'Gift Cards')]"));
